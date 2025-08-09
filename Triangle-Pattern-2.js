6ec9f1754e4e9ac64777c076c0b9b0d5b0cf9d74
Daniyal Ahmed <da9033431@gmail.com>
2025-08-09 23:05:15 +0100

    problem 2 minor fixes

--- Triangle-Pattern-2.js
+++ Triangle-Pattern-2.js
@@ -2,18 +2,15 @@ function Pattern(n) {
   for (let row = 1; row <= n; row++) {
     let star = "";
 
-    let spaces = n - row
-    for (let j = 1; j = spaces; spaces--) {
+    let spaces = n - row;
+    for (let j = 1; (j = spaces); spaces--) {
       star = star + " ";
     }
 
-    for (let stars = 1; stars <= row; stars++) {
+    for (let stars = 1; stars <= 2 * row - 1; stars++) {
       star = star + "*";
     }
 
-    for (let stars = 2; stars <= row; stars++) {
-      star = star + "*";
-    }
     console.log(star);
   }
 }
