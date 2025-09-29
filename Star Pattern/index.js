a6eaec4683c2f3b05dab2b775a57788f1aa15802
Daniyal Ahmed <da9033431@gmail.com>
2025-09-29 18:14:55 +0100

    solving problems

--- Star Pattern/index.js
+++ Star Pattern/index.js
@@ -1,23 +1,103 @@
-function StarPatternOne(n) {
-  for (let i = 0; i < n; i++) {
-    let row = "";
-    for (let j = 0; j < n; j++) {
-      row += "*";
-    }
-    console.log(row);
-  }
-}
+// function StarPatternOne(n) {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < n; j++) {
+//       row += "*";
+//     }
+//     console.log(row);
+//   }
+// }
 
-StarPatternOne(6);
+// StarPatternOne(6);
 
-function StarPatternTwo(n) {
+// function StarPatternTwo(n) {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < i + 1; j++) {
+//       row += "*";
+//     }
+//     console.log(row);
+//   }
+// }
+
+// StarPatternTwo(6);
+
+// function StarPatternThree(n) {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < i + 1; j++) {
+//       row += i + 1;
+//     }
+//     console.log(row);
+//   }
+// }
+
+// StarPatternThree(6);
+
+// function StarPatternFour(n) {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < n - i; j++) {
+//       row += j + 1;
+//     }
+//     console.log(row);
+//   }
+// }
+
+// StarPatternFour(6);
+
+// function StarPatternFive(n) {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     for (let j = 0; j < n - i; j++) {
+//       row += " ";
+//     }
+
+//     for (let j = 0; j < i + 1; j++) {
+//       row += "*";
+//     }
+
+//     console.log(row);
+//   }
+// }
+
+// StarPatternFive(5);
+
+// function StarPatternSix(n) {
+//   for (let i = 0; i < n; i++) {
+//     let row = "";
+//     let switchVar = 1;
+//     for (let j = 0; j < i + 1; j++) {
+//       row += switchVar;
+//      if (switchVar === 0) {
+//         switchVar = 1;
+//       } else {
+//         switchVar = 0;
+//       }
+//     }
+
+//     console.log(row);
+//   }
+// }
+
+// StarPatternSix(5);
+
+function StarPatternSeven(n) {
+  let switchVar = 0;
+
   for (let i = 0; i < n; i++) {
     let row = "";
     for (let j = 0; j < i + 1; j++) {
-      row += "*";
+      if (switchVar === 0) {
+        switchVar = 1;
+      } else {
+        switchVar = 0;
+      }
+      row += switchVar;
     }
+
     console.log(row);
   }
 }
 
-StarPatternTwo(6);
+StarPatternSeven(5);
