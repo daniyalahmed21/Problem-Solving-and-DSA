df806d366a90caed70d8a6f5bbc37d8236ee3591
Daniyal Ahmed <da9033431@gmail.com>
2025-10-02 15:24:21 +0100

    solving problems

--- Arrays/Merge Sorted Array/index.js
+++ Arrays/Merge Sorted Array/index.js
@@ -1,35 +1,41 @@
 var merge = function (nums1, m, nums2, n) {
-  let p1 = 0;
-  let p2 = 0;
-  let nums3 = [];
+  let p1 = 0; // pointer for nums1copy
+  let p2 = 0; // pointer for nums2
+  let p = 0;  // pointer for writing into nums1
+
+  let nums1copy = nums1.slice(0, m);
 
   while (p1 < m && p2 < n) {
-    if (nums1[p1] < nums2[p2]) {
-      nums3.push(nums1[p1]);
+    if (nums1copy[p1] <= nums2[p2]) {
+      nums1[p] = nums1copy[p1];
       p1++;
     } else {
-      nums3.push(nums2[p2]);
+      nums1[p] = nums2[p2];
       p2++;
     }
+    p++;
   }
 
-  // Add remaining elements
+  // Add remaining elements from nums1copy
   while (p1 < m) {
-    nums3.push(nums1[p1]);
+    nums1[p] = nums1copy[p1];
     p1++;
+    p++;
   }
 
+  // Add remaining elements from nums2
   while (p2 < n) {
-    nums3.push(nums2[p2]);
+    nums1[p] = nums2[p2];
     p2++;
+    p++;
   }
-
-  return nums3;
 };
 
 
+let nums1 = [1, 2, 3, 0, 0, 0],
+  m = 3,
+  nums2 = [2, 5, 6],
+  n = 3;
 
-
-let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-
-console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
+merge(nums1, m, nums2, n);
+console.log(nums1);
