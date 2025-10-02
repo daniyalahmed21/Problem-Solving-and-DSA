9256c1c2481daa72bfcd5c53da82fe74b5ed7696
Daniyal Ahmed <da9033431@gmail.com>
2025-10-02 17:52:42 +0100

    solving problems

--- Arrays/Move Zeros/index.js
+++ Arrays/Move Zeros/index.js
@@ -1,23 +1,22 @@
-var moveZeroes = function (nums) {
-  let p1 = 0;
-  let p2 = 1;
+var moveZeroes = function(nums) {
+  let lastNonZeroFoundAt = 0;
 
-  while (p1 <= nums.length - 1 && p2 <= nums.length - 1) {
-    if (nums[p1] === 0) {
-      if (nums[p2] !== 0) {
-        nums[p1] = nums[p2];
-        nums[p2] = 0;
-      } else {
-        p2++;
-      }
-    } else {
-      p1++;
-      p2++
+  // Move all non-zero numbers forward
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[lastNonZeroFoundAt] = nums[i];
+      lastNonZeroFoundAt++;
     }
+  }
+
+  // Fill remaining positions with zeros
+  for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
+    nums[i] = 0;
   }
 };
 
-let nums = [4,2,4,0,0,3,0,5,1,0]
+
+let nums = [4, 2, 4, 0, 0, 3, 0, 5, 1, 0];
 
 moveZeroes(nums);
 console.log(nums);
