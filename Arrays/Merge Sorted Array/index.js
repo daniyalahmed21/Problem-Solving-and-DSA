675bd822362c3eaa0fa97a0c9d39266c21a6a885
Daniyal Ahmed <da9033431@gmail.com>
2025-10-02 16:49:35 +0100

    solving problems

--- Arrays/Merge Sorted Array/index.js
+++ Arrays/Merge Sorted Array/index.js
@@ -1,38 +1,27 @@
 var merge = function (nums1, m, nums2, n) {
-  let p1 = 0; // pointer for nums1copy
-  let p2 = 0; // pointer for nums2
-  let p = 0;  // pointer for writing into nums1
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let p = n + m - 1;
 
-  let nums1copy = nums1.slice(0, m);
-
-  while (p1 < m && p2 < n) {
-    if (nums1copy[p1] <= nums2[p2]) {
-      nums1[p] = nums1copy[p1];
-      p1++;
-    } else {
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums2[p2] > nums1[p1]) {
       nums1[p] = nums2[p2];
-      p2++;
+      p2--;
+    } else {
+      nums1[p] = nums1[p1];
+      p1--;
     }
-    p++;
+    p--;
   }
 
-  // Add remaining elements from nums1copy
-  while (p1 < m) {
-    nums1[p] = nums1copy[p1];
-    p1++;
-    p++;
-  }
-
-  // Add remaining elements from nums2
-  while (p2 < n) {
-    nums1[p] = nums2[p2];
-    p2++;
-    p++;
+  while(p2 >= 0) {
+    nums1[p] = nums2[p2]
+    p2--
+    p--
   }
 };
 
-
-let nums1 = [1, 2, 3, 0, 0, 0],
+let nums1 = [1, 2, 5, 0, 0, 0],
   m = 3,
   nums2 = [2, 5, 6],
   n = 3;
