f986616c3161ad39da5d683d87f58e296a028ce9
Daniyal Ahmed <da9033431@gmail.com>
2025-10-14 22:38:34 +0100

    solving problems

--- Linked List/Linked List Cycle/index.js
+++ Linked List/Linked List Cycle/index.js
@@ -1,15 +1,32 @@
-var hasCycle = function(head) {
-    const seenNodes = new Set();
-    let curr = head
+// var hasCycle = function(head) {
+//     const seenNodes = new Set();
+//     let curr = head
 
-    while (curr != null){
-        if(seenNodes.has(curr)){
-            return true 
-        }
+//     while (curr != null){
+//         if(seenNodes.has(curr)){
+//             return true
+//         }
 
-        seenNodes.add(curr)
-        curr = curr.next;
+//         seenNodes.add(curr)
+//         curr = curr.next;
+//     }
+
+//     return false
+// };
+
+var hasCycle = function (head) {
+  if (!head) return false;
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (fast === null || fast.next === null) {
+      return false;
     }
 
-    return false
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return true;
 };
