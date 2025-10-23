701d004f78951783390d5ec91785b5ef02905414
Daniyal Ahmed <da9033431@gmail.com>
2025-10-23 22:00:52 +0100

    solving problems

--- Linked List/Swap Pairs/index.js
+++ Linked List/Swap Pairs/index.js
@@ -1,22 +1,31 @@
+// var swapPairs = function (head) {
+
+//     const dummy = new ListNode(0);
+//     dummy.next = head;
+//     let prev = dummy;
+
+//     while (head && head.next) {
+//         let first = head;
+//         let second = head.next;
+
+//         first.next = second.next;
+//         second.next = first;
+//         prev.next = second;
+
+//         prev = first;
+//         head = first.next;
+//     }
+//     return dummy.next;
+// };
+
+
 var swapPairs = function (head) {
+    if (!head || !head.next) return head;
 
-    const dummy = new ListNode(0);
-    dummy.next = head;
-    let prev = dummy;
+    let first = head;
+    let second = head.next;
+    first.next = swapPairs(second.next);
+    second.next = first
 
-    while (head && head.next) {
-        let first = head;
-        let second = head.next;
-
-        first.next = second.next;
-        second.next = first;
-        prev.next = second;
-
-        prev = first;
-        head = first.next;
-    }
-    return dummy.next;
+    return second;
 };
-
-
-
