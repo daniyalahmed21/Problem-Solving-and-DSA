6222da8ab741eff1478df2d26324b77593c6ff29
Daniyal Ahmed <da9033431@gmail.com>
2025-10-31 12:00:00 +0000

    Solving problems

--- Strings/Jewels and stones/index.js
+++ Strings/Jewels and stones/index.js
@@ -1,8 +1,25 @@
+// var numJewelsInStones = function(jewels, stones) {
+//     let occurrences = 0 ;
+//     for (let i = 0; i < stones.length; i++) {
+//         if(jewels.includes(stones[i] )){
+//             occurrences ++ 
+//         }
+//     }
+
+//     return occurrences;
+// };
+
 var numJewelsInStones = function(jewels, stones) {
+    let jewelSet = new Set()
     let occurrences = 0 ;
+
+    for (let i = 0; i < jewels.length; i++) {
+        jewelSet.add(jewels[i])
+    }
+
     for (let i = 0; i < stones.length; i++) {
-        if(jewels.includes(stones[i] )){
-            occurrences ++ 
+        if(jewelSet.has(stones[i])){
+            occurrences++
         }
     }
 
