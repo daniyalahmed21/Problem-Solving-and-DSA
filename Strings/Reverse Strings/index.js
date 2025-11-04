91d1ed0c3ed38a787a396652a945886a99f9e169
Daniyal Ahmed <da9033431@gmail.com>
2025-11-04 21:30:44 +0000

    Solving problems

---
+++ Strings/Reverse Strings/index.js
@@ -0,0 +1,21 @@
+var reverseStr = function (s, k) {
+  let arr = s.split("");
+  let n = arr.length;
+
+  for (let i = 0; i < n; i = i + 2 * k) {
+    let left = i;
+    let right = Math.min(i + k - 1, n - 1);
+    while (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+
+  return arr.join("");
+};
+
+let s = "abcdefg",
+  k = 2;
+
+console.log(reverseStr(s, k));
