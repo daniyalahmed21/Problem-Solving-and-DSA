d9c2db6da7fc06afb993f40d948ca7fa563580ef
Daniyal Ahmed <da9033431@gmail.com>
2025-11-04 21:39:20 +0000

    Solving problems

--- Strings/Reverse Strings/index.js
+++ Strings/Reverse Strings/index.js
@@ -5,10 +5,10 @@ var reverseStr = function (s, k) {
   for (let i = 0; i < n; i = i + 2 * k) {
     let left = i;
     let right = Math.min(i + k - 1, n - 1);
-    while (left < right) {
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-      left++;
-      right--;
+    while(left < right){
+        [arr[left],arr[right]] = [arr[right],arr[left]]
+        left++
+        right--
     }
   }
 
