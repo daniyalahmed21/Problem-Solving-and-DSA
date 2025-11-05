6a331f543dd915ab4db59b96bbcd25010726fb97
Daniyal Ahmed <da9033431@gmail.com>
2025-11-05 14:04:54 +0000

    Solving Problems

--- Strings/Group Anagrams/index.js
+++ Strings/Group Anagrams/index.js
@@ -1,18 +1,41 @@
-var groupAnagrams = function(strs) {
-    let map = {}
-    for (let i = 0; i < strs.length; i++) {
-        let word = strs[i];
-        let sortedWord = word.split('').sort().join('');
+// var groupAnagrams = function(strs) {
+//     let map = {}
+//     for (let i = 0; i < strs.length; i++) {
+//         let word = strs[i];
+//         let sortedWord = word.split('').sort().join('');
 
-        if (map[sortedWord]) {
-            map[sortedWord].push(word);
-        } else {
-            map[sortedWord] = [word];
-        }
+//         if (map[sortedWord]) {
+//             map[sortedWord].push(word);
+//         } else {
+//             map[sortedWord] = [word];
+//         }
+//     }
+//     return Object.values(map);
+// };
+
+var groupAnagrams = function (strs) {
+  let map = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    let freqArr = Array(26).fill(0);
+    let s = strs[i];
+    for (let j = 0; j < s.length; j++) {
+      let index = s[j].charCodeAt() - "a".charCodeAt();
+      ++freqArr[index];
     }
-    return Object.values(map);
+    
+    let key = freqArr.join('#'); // Use '#' or ',' as delimiter (safer than raw join)
+    
+    if (!map[key]) {
+      map[key] = [s];
+    } else {
+      map[key].push(s);
+    }
+  }
+
+  return Object.values(map)
 };
 
-let strs = ["eat","tea","tan","ate","nat","bat"]
+let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
-console.log(groupAnagrams(strs))
+console.log(groupAnagrams(strs));
