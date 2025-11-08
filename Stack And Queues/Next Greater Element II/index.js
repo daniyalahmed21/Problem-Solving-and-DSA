ebd20a3e86772bdde9a4d85c0b03b74412023e53
Daniyal Ahmed <da9033431@gmail.com>
2025-11-08 16:02:16 +0000

    Solving Problems

--- Stack And Queues/Next Greater Element II/index.js
+++ Stack And Queues/Next Greater Element II/index.js
@@ -1,18 +1,42 @@
-var nextGreaterElements = function (nums) {
-  let n = nums.length;
-  let ans = [];
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (nums[i] < nums[(i + j) % n]) {
-        ans.push(nums[(i + j) % n]);
+// var nextGreaterElements = function (nums) {
+//   let n = nums.length;
+//   let ans = [];
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       if (nums[i] < nums[(i + j) % n]) {
+//         ans.push(nums[(i + j) % n]);
+//         break;
+//       }
+//     }
+//     if (ans.length === i ) {
+//       ans.push(-1);
+//     }
+//   }
+//   return ans;
+// };
+
+var nextGreaterElements = function (arr) {
+  let n = arr.length;
+  let ans = Array(n).fill(-1);
+
+  let stack = [];
+
+  stack.push(arr[n - 1]);
+
+  for (let i = (n*2) - 2; i >= 0; i--) {
+    while (stack.length) {
+      let top = stack[stack.length - 1];
+      if (arr[i % n] < top) {
+        ans[i % n] = top;
         break;
+      } else {
+        stack.pop();
       }
     }
-    if (ans.length === i ) {
-      ans.push(-1);
-    }
+    stack.push(arr[i % n]);
   }
-  return ans;
+
+  return ans.slice(0, n);
 };
 
 // Example usage:
