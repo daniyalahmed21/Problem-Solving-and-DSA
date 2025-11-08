da27c71797bd61a75515def51105b16b093f7e24
Daniyal Ahmed <da9033431@gmail.com>
2025-11-08 12:02:44 +0000

    Solving Problems

--- Stack And Queues/Next Greater Element/index.js
+++ Stack And Queues/Next Greater Element/index.js
@@ -1,39 +1,67 @@
+// var nextGreaterElement = function (nums1, nums2) {
+//   let stack = [];
+//   let nge = [];
+//   let ans = [];
+//   let nums2copy = [...nums2];
+//   while (nums2.length > 0) {
+//     let top = stack[stack.length - 1];
+//     if (stack.length === 0) {
+//       nge.push(-1);
+//       stack.push(nums2[nums2.length - 1]);
+//       nums2.pop();
+//     } else if (nums2[nums2.length - 1] < top) {
+//       nge.push(top);
+//       stack.push(nums2[nums2.length - 1]);
+//       nums2.pop();
+//     } else if (nums2[nums2.length - 1] > top) {
+//       if (stack.length > 0) {
+//         stack.pop();
+//       } else {
+//         nge.push(-1);
+//         stack.push(top);
+//       }
+//     }
+//   }
+
+//   nge.reverse();
+
+//   while (nums1.length > 0) {
+//     let top = nums1[nums1.length - 1];
+
+//     let index = nums2copy.indexOf(top);
+//     ans.push(nge[index]);
+//     nums1.pop();
+//   }
+
+//   return ans.reverse();
+// };
+
 var nextGreaterElement = function (nums1, nums2) {
   let stack = [];
-  let nge = [];
-  let ans = [];
-  let nums2copy = [...nums2];
-  while (nums2.length > 0) {
-    let top = stack[stack.length - 1];
-    if (stack.length === 0) {
-      nge.push(-1);
-      stack.push(nums2[nums2.length - 1]);
-      nums2.pop();
-    } else if (nums2[nums2.length - 1] < top) {
-      nge.push(top);
-      stack.push(nums2[nums2.length - 1]);
-      nums2.pop();
-    } else if (nums2[nums2.length - 1] > top) {
-      if (stack.length > 0) {
-        stack.pop();
+  let ngeMap = {};
+  let n = nums2.length;
+
+  stack.push(nums2[n - 1]);
+  ngeMap[nums2[n - 1]] = -1;
+
+  for (let i = n - 2; i < n; i--) {
+    while (stack.length) {
+      if (stack[stack.length - 1] > nums2[i]) {
+        stack.push(nums2[i]);
+        ngeMap[nums2[i]] = stack[stack.length - 1];
       } else {
-        nge.push(-1);
-        stack.push(top);
+        stack.pop();
       }
     }
+
+    if (stack.length === 0) {
+      ngeMap[nums2[i]] = -1;
+    }
+
+    stack.push(nums2[i]);
   }
 
-  nge.reverse();
-
-  while (nums1.length > 0) {
-    let top = nums1[nums1.length - 1];
-
-    let index = nums2copy.indexOf(top);
-    ans.push(nge[index]);
-    nums1.pop();
-  }
-
-  return ans.reverse();
+  return nums1.map(x=> ngeMap[x])
 };
 
 let nums1 = [4, 1, 2],
