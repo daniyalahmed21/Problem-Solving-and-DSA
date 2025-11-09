811c7d570b4d8e95d81f1ea26cd8675d780b61ac
Daniyal Ahmed <da9033431@gmail.com>
2025-11-09 12:41:42 +0000

    Solving Problems

---
+++ Binary Search Algorithm/Guess Game/index.js
@@ -0,0 +1,36 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let left = 1;
+  let right = n;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    let res = guess(mid)
+    if (res === 0) {
+      return mid;
+    }
+
+    if (res === 1) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+};
+
+let n = 10,
+  pick = 6;
+console.log(guessNumber(n));
