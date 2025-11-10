2e104a3ded7b8a6d8e183672609b2d4950554be6
Daniyal Ahmed <da9033431@gmail.com>
2025-11-10 12:35:31 +0000

    Solving Problems

--- Binary Search Algorithm/First Bad Version/index.js
+++ Binary Search Algorithm/First Bad Version/index.js
@@ -20,13 +20,8 @@ var solution = function (isBadVersion) {
     return function (n) {
         let left = 1;
         let right = n;
-        let mid;
         while (left <= right) {
-            mid = left + Math.floor((right - left) / 2);
-
-            if (right === left) {
-                break;
-            }
+            let mid = left + Math.floor((right - left) / 2);
             
             if (!isBadVersion(mid)) {
                 left = mid + 1
@@ -34,6 +29,6 @@ var solution = function (isBadVersion) {
                 right = mid
             }
         }
-        return mid
+        return right
     };
 };
