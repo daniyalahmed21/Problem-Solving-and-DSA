25e28fe9c5819b05e3317d089b7b05fe190b82cb
Daniyal Ahmed <da9033431@gmail.com>
2025-11-14 12:00:00 +0000

    Solving Problems

---
+++ Binary Search Algorithm/find the starting and ending position/index.js
@@ -0,0 +1,41 @@
+var searchRange = function (nums, target) {
+  let right = nums.length - 1;
+  let left = 0;
+  let ans = [-1, -1];
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  nums[right] === target ? (ans[0] = right) : -1;
+
+  right = nums.length - 1;
+  left = 0;
+
+  while (left < right) {
+    let mid = left + Math.ceil((right - left) / 2);
+    console.log(left, mid, right);
+
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid;
+    }
+
+    if (nums[right] !== target) {
+      right = right - 1;
+    }
+  }
+
+  nums[left] === target ? (ans[1] = left) : -1;
+
+  return ans;
+};
+
+let nums = [5, 7, 7, 8, 8, 10];
+console.log(searchRange(nums, 8));
