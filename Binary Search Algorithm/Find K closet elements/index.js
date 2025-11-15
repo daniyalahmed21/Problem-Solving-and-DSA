04617f1bc30e7377651157a9b4e638089a80f29f
Daniyal Ahmed <da9033431@gmail.com>
2025-11-15 20:53:56 +0000

    Solving Problems

---
+++ Binary Search Algorithm/Find K closet elements/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let left = 0;
+  let right = arr.length - k;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return arr.slice(left, left + k);
+};
+
+let arr = [1, 1, 2, 3, 4, 5],
+  k = 4,
+  x = -1;
+console.log(findClosestElements(arr, k, x));
