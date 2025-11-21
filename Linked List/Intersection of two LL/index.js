a14ecc267ac1c30d7a393c6c58cd46317028de05
Daniyal Ahmed <da9033431@gmail.com>
2025-11-21 19:02:43 +0000

    Solving Problems

--- Linked List/Intersection of two LL/index.js
+++ Linked List/Intersection of two LL/index.js
@@ -18,3 +18,44 @@ var getIntersectionNode = function (headA, headB) {
 
   return null;
 };
+
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null;
+
+  let currentA = headA;
+  let currentB = headB;
+
+  let n = 0;
+  while (currentA) {
+    n++;
+    currentA = currentA.next;
+  }
+
+  let m = 0;
+  while (currentB) {
+    m++;
+    currentB = currentB.next;
+  }
+
+  let diff = Math.abs(n- m);
+
+  if(n>m){
+    let temp = headA
+    headA = headB
+    headB = temp
+  }
+
+  for (let i = 0; i < diff; i++) {
+   headB = headB.next
+  }
+
+  currentA = headA;
+  currentB = headB;
+
+  while(currentA !== currentB){
+    currentA = currentA.next
+    currentB = currentB.next
+  }
+
+  return currentA
+};
