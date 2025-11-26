81e7a1ccaf1bd7e078dc9399b0b13a3820c22a3d
Daniyal Ahmed <da9033431@gmail.com>
2025-11-26 11:42:30 +0000

    Solving Problems

---
+++ Sliding Window/239 Sliding Window Maximum/index.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+    let queue = []
+    let max = []
+    let i = 0
+    let j = 0
+
+    while (j < nums.length) {
+        while (queue.length && nums[j] > queue[queue.length - 1]) {
+            queue.pop()
+        }
+
+        queue.push(nums[j])
+
+
+        if (j >= k - 1) {
+            max.push(queue[0])
+
+            if(nums[i] === queue[0]){
+                queue.shift()
+            }
+
+            i++
+        }
+
+        j++
+    }
+    return max
+
+};
