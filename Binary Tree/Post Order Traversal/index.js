b5f5883999c0a970d7447b7b0ef47e3392bcd2c1
Daniyal Ahmed <da9033431@gmail.com>
2025-11-27 15:57:23 +0000

    Solving Problems

--- Binary Tree/Post Order Traversal/index.js
+++ Binary Tree/Post Order Traversal/index.js
@@ -15,19 +15,47 @@
 // };
 
 
+// var postorderTraversal = function(root) {
+//     if (!root) return [];
+
+//     let ans = [];
+//     let s1 = [root];
+//     let s2 = []
+
+//     while (s1.length > 0) {
+//         let curr = s1.pop();
+//         s2.push(curr.val)
+//         curr.left && s1.push(curr.left); 
+//         curr.right && s1.push(curr.right);
+//     }
+
+//     return s2.reverse();
+// };
 var postorderTraversal = function(root) {
     if (!root) return [];
 
     let ans = [];
-    let s1 = [root];
-    let s2 = []
+    let stack = [];
+    let curr = root;
+    let lastVisited = null;
 
-    while (s1.length > 0) {
-        let curr = s1.pop();
-        s2.push(curr.val)
-        curr.left && s1.push(curr.left); 
-        curr.right && s1.push(curr.right);
+    while (curr || stack.length > 0) {
+        while (curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+
+        let peakNode = stack[stack.length - 1];
+
+        // go right if right not processed yet
+        if (peakNode.right && lastVisited !== peakNode.right) {
+            curr = peakNode.right;
+        } else {
+            ans.push(peakNode.val);
+            lastVisited = peakNode;
+            stack.pop();
+        }
     }
 
-    return s2.reverse();
+    return ans;
 };
