90272e86bbf26b7a65ec6a2d2d195abc66b0da0b
Daniyal Ahmed <da9033431@gmail.com>
2025-11-27 12:04:58 +0000

    Solving Problems

--- Binary Tree/Pre Order Traversal/index.js
+++ Binary Tree/Pre Order Traversal/index.js
@@ -10,20 +10,49 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+// var preorderTraversal = function (root) {
+//   let ans = [];
+
+//   function Traversal(currRoot) {
+//     ans.push(currRoot.val);
+//     if (currRoot.left) {
+//       Traversal(currRoot.left);
+//     }
+//     if (currRoot.right) {
+//       Traversal(currRoot.right);
+//     }
+//   }
+
+//   root && Traversal(root);
+
+//   return ans;
+// };
+
 var preorderTraversal = function (root) {
   let ans = [];
-
-  function Traversal(currRoot) {
-    ans.push(currRoot.val);
-    if (currRoot.left) {
-      Traversal(currRoot.left);
-    }
-    if (currRoot.right) {
-      Traversal(currRoot.right);
+  let stack = [];
+  if (!root) {
+    return [];
+  }
+  stack.push(root[0]);
+  while (stack.length >= 1) {
+    console.log(stack);
+    let curr = stack.pop();
+    console.log(curr);
+    if (curr) {
+      if (curr && curr.right) {
+        stack.push(curr.right);
+      }
+      if (curr && curr.left) {
+        stack.push(curr.left);
+      }
+      ans.push(curr.val);
     }
   }
 
-  root && Traversal(root);
-
   return ans;
 };
+
+let root = [1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9];
+
+console.log(preorderTraversal(root));
