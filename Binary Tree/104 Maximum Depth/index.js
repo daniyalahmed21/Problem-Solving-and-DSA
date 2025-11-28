75fd11b799936ceb24af724b4dc22c23dcc970f6
Daniyal Ahmed <da9033431@gmail.com>
2025-11-28 12:59:22 +0000

    Solving Problems

--- Binary Tree/104 Maximum Depth/index.js
+++ Binary Tree/104 Maximum Depth/index.js
@@ -1,17 +1,25 @@
 var maxDepth = function (root) {
-    let maxDepth = 0
-    let ans = [];
+  let maxDepth = 0;
+  let ans = [];
 
-    function traverse(node, level) {
-        if (!node) return;
+  function traverse(node, level) {
+    if (!node) return;
 
-        if (!ans[level]) ans[level] = [];
+    if (!ans[level]) ans[level] = [];
 
-        ans[level].push(node.val);
+    ans[level].push(node.val);
 
-        traverse(node.left, level + 1);
-        traverse(node.right, level + 1);
-    }
-    traverse(root, 0)
-    return ans.length ;
+    traverse(node.left, level + 1);
+    traverse(node.right, level + 1);
+  }
+  traverse(root, 0);
+  return ans.length;
+};
+
+var maxDepth = function (root) {
+    if(!root) return 0
+  let leftMax = maxDepth(root.left);
+  let rightMax = maxDepth(root.right);
+
+  return 1 + Math.max(leftMax, rightMax);
 };
