19a4cf4e3662a99820c2730f8a05a1a80de961e9
Daniyal Ahmed <da9033431@gmail.com>
2025-11-28 11:31:34 +0000

    Solving Problems

--- Binary Tree/102 Level order traversal/index.js
+++ Binary Tree/102 Level order traversal/index.js
@@ -1,17 +1,38 @@
+// var levelOrder = function (root) {
+//     if (!root) return []
+//     let queue = [root]
+//     let ans = []
+//     while (queue.length > 0) {
+//         let level = []
+//         let levelSize = queue.length
+//         for (let i = 0; i < levelSize; i++) {
+//             let curr = queue.shift()
+//             curr.left && queue.push(curr.left)
+//             curr.right && queue.push(curr.right)
+//             level.push(curr.val)
+//         }
+//         ans.push(level)
+//     }
+//     return ans
+// };
+
+
 var levelOrder = function (root) {
-    if (!root) return []
-    let queue = [root]
-    let ans = []
-    while (queue.length > 0) {
-        let level = []
-        let levelSize = queue.length
-        for (let i = 0; i < levelSize; i++) {
-            let curr = queue.shift()
-            curr.left && queue.push(curr.left)
-            curr.right && queue.push(curr.right)
-            level.push(curr.val)
-        }
-        ans.push(level)
+    if (!root) return [];
+
+    let ans = [];
+
+    function traverse(node, level) {
+        if (!node) return;
+
+        if (!ans[level]) ans[level] = [];
+
+        ans[level].push(node.val);
+
+        traverse(node.left, level + 1);
+        traverse(node.right, level + 1);
     }
-    return ans
+
+    traverse(root, 0);
+    return ans;
 };
