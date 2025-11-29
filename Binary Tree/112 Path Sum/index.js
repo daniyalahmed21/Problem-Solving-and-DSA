287b16393807ce6b763ee75024e0af0c37650e29
Daniyal Ahmed <da9033431@gmail.com>
2025-11-29 12:22:21 +0000

    Solving Problems

--- Binary Tree/112 Path Sum/index.js
+++ Binary Tree/112 Path Sum/index.js
@@ -1,18 +1,29 @@
 var hasPathSum = function (root, targetSum) {
-     if (!root) return false
-    let ans = false
-    function Traverse(curr, prevSum) {
-        let newSum = prevSum + curr.val
+  if (!root) return false;
+  let ans = false;
+  function Traverse(curr, prevSum) {
+    let newSum = prevSum + curr.val;
 
-        if (!curr.left && !curr.right && newSum === targetSum) {
-            ans = true
-        }
-
-        curr.left && Traverse(curr.left, newSum)
-        curr.right && Traverse(curr.right, newSum)
+    if (!curr.left && !curr.right && newSum === targetSum) {
+      ans = true;
     }
 
-    Traverse(root, 0)
+    curr.left && Traverse(curr.left, newSum);
+    curr.right && Traverse(curr.right, newSum);
+  }
 
-    return ans
+  Traverse(root, 0);
+
+  return ans;
+};
+
+var hasPathSum = function (root, targetSum) {
+  if (!root) return false;
+  if (!root.left && !root.right) {
+    return root.val === targetSum;
+  }
+
+  let leftSubTreeHasPathSum = hasPathSum(root.left, targetSum - root.val);
+  let rightSubTreeHasPathSum = hasPathSum(root.right, targetSum - root.val);
+  return leftSubTreeHasPathSum || rightSubTreeHasPathSum;
 };
