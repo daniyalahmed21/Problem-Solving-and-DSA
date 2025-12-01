8db5566d8eeb403217995af8af144ec8b7df88c9
Daniyal Ahmed <da9033431@gmail.com>
2025-12-01 11:14:42 +0000

    Solving Problems

---
+++ Binary Tree/572 Subtree of another tree/index.js
@@ -0,0 +1,18 @@
+var isSubtree = function (root, subRoot) {
+    if (!root) return false;
+
+    function isSameTree(p, q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+
+        return (
+            p.val === q.val &&
+            isSameTree(p.left, q.left) &&
+            isSameTree(p.right, q.right)
+        );
+    }
+
+    if (isSameTree(root, subRoot)) return true;
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
