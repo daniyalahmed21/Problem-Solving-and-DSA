db8046ce8e9ac9bc7e4cfdff02130628baf8d894
Daniyal Ahmed <da9033431@gmail.com>
2025-12-03 11:44:33 +0000

    Refactor rightSideView function for improved readability and consistency

--- Binary Tree/199 Right Side View/inidex.js
+++ Binary Tree/199 Right Side View/inidex.js
@@ -1,20 +1,38 @@
 var rightSideView = function (root) {
-    if (!root) return [];
+  if (!root) return [];
 
-    let levels = [];
+  let levels = [];
 
-    function Traverse(node, level) {
-        if (!node) return;
+  function Traverse(node, level) {
+    if (!node) return;
 
-        if (!levels[level]) levels[level] = [];
-        levels[level].push(node.val);
+    if (!levels[level]) levels[level] = [];
+    levels[level].push(node.val);
 
-        Traverse(node.left, level + 1);
-        Traverse(node.right, level + 1);
+    Traverse(node.left, level + 1);
+    Traverse(node.right, level + 1);
+  }
+
+  Traverse(root, 0);
+
+  // take the **last element** of each level
+  return levels.map((level) => level[level.length - 1]);
+};
+
+
+var rightSideView = function (root) {
+  if (!root) return [];
+  let ans = [];
+  let q = [root];
+  while (q.length) {
+    let levelSize = q.length;
+    for (let i = 0; i < levelSize; i++) {
+      let curr = q.shift();
+
+      i === 0 && ans.push(curr.val);
+      curr.right && q.push(curr.right);
+      curr.left && q.push(curr.left);
     }
-
-    Traverse(root, 0);
-
-    // take the **last element** of each level
-    return levels.map(level => level[level.length - 1]);
+  }
+  return ans
 };
