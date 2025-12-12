23b64d36e8cb948c9b382d9ce4a9586dad77f6c1
Daniyal Ahmed <da9033431@gmail.com>
2025-12-12 19:24:39 +0000

    Solving Problems

--- Linked List/Linked List Cycle/index.js
+++ Linked List/Linked List Cycle/index.js
@@ -30,3 +30,29 @@ var hasCycle = function (head) {
 
   return true;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    let curr = head
+    let set = new Set()
+    while (curr) {
+        if (set.has(curr)) {
+            return true
+        }
+        set.add(curr)
+        curr = curr.next
+    }
+
+    return false
+};
